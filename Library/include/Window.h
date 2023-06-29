1c60634f06faccff064332d954e73dac2c4b3e34
nesmy <pnesmy@gmail.com>
2023-06-28 22:50:41 -0400

    imgui

--- Library/include/Window.h
+++ Library/include/Window.h
@@ -2,6 +2,11 @@
 
 #include "pch.h"
 
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
 namespace Small {
