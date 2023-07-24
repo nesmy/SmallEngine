650ebff3229563de2860541b27b92a85efee5f89
nesmy <pnesmy@gmail.com>
2023-07-24 16:33:36 -0400

    launcher 0.1

---
+++ Launcher/include/Window.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "pch.h"
+
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "Shader.h"
+
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+namespace Small {
+
+    struct WindowProps
+    {
+        std::string Title;
+        unsigned int Width;
+        unsigned int Height;
+
+        WindowProps(const std::string& title = "Small Engine",
+                    unsigned int width = 1280,
+                    unsigned int height = 720)
+            : Title(title), Width(width), Height(height)
+            {
+
+            }
+    };
+
+    class Window {
+        public:
+        Window();
+        ~Window();
+
+        void Update();
+        void Render();
+
+        private:
+        GLFWwindow* window;
+        
+    };
+
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+}
